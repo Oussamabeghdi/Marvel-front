aeb5756fc0ee56019b33c14db476e707130004ec
oussamab <oussama@MacBook-Pro-de-oussamab.local>
2023-03-01 21:54:46 +0100

    change component

--- src/pages/Characters.js
+++ src/pages/Characters.js
@@ -3,37 +3,37 @@ import axios from "axios";
 import Details from "../components/Details";
 // import { Link } from "react-router-dom";
 
-const Characters = ({ search }) => {
-  const [data, setData] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+// const Characters = ({ search }) => {
+//   const [data, setData] = useState([]);
+//   const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(
-          `site--marvel-backend--9gtnl5qyn2yw.code.run/characters?name=${search}`
-        );
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         const response = await axios.get(
+//           `site--marvel-backend--9gtnl5qyn2yw.code.run/characters?name=${search}`
+//         );
 
-        console.log(response.data);
-        setData(response.data);
-        setIsLoading(false);
-      } catch (error) {
-        console.log(error.message);
-      }
-    };
+//         console.log(response.data);
+//         setData(response.data);
+//         setIsLoading(false);
+//       } catch (error) {
+//         console.log(error.message);
+//       }
+//     };
 
-    fetchData();
-  }, [search]);
+//     fetchData();
+//   }, [search]);
 
-  return isLoading ? (
-    <p>Loading...</p>
-  ) : (
-    <div className="container">
-      {data.results.map((item) => {
-        return <Details key={item._id} item={item} />;
-      })}
-    </div>
-  );
-};
+//   return isLoading ? (
+//     <p>Loading...</p>
+//   ) : (
+//     <div className="container">
+//       {data.results.map((item) => {
+//         return <Details key={item._id} item={item} />;
+//       })}
+//     </div>
+//   );
+// };
 
-export default Characters;
+// export default Characters;
