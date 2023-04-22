b38c07dc20823b50682780f0abb5184cc9bc8874
oussamab <oussama@MacBook-Pro-de-oussamab.local>
2023-04-22 20:01:01 +0200

    change charactercard-component

--- src/components/Comicscard.js
+++ src/components/Comicscard.js
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { ReactComponent as Heart } from "../assets/svg/heart.svg";
 
 const Comicscard = ({ item }) => {
   const [isFavorite, setIsFavorite] = useState(false);
@@ -38,31 +39,37 @@ const Comicscard = ({ item }) => {
     }
   };
   return (
-    <article className="comics">
-      <Link to={`/comic/${item._id}`}>
-        <div>
-          <p className="comics-title ">{item.title}</p>
-          {/* <p className="comics-description ">{item.description}</p> */}
-        </div>
-        <div className="comics-image-container">
-          <img
-            className="comics-image"
-            src={
-              item.thumbnail.path +
-              "/portrait_medium" +
-              "." +
-              item.thumbnail.extension
-            }
-            alt="comics "
-          />
-        </div>
-      </Link>
-      <button
+    // <article className="comics">
+    <section>
+      <div className="card-wrapper">
+        <Link className="character-link" to={`/comic/${item._id}`}>
+          <div className="details-wrapper">
+            <p className="comics-title ">{item.title}</p>
+
+            {/* </div> */}
+            {/* <div className="comics-image-container"> */}
+            <img
+              className="comics-image"
+              src={item.thumbnail.path + "." + item.thumbnail.extension}
+              alt="comics "
+            />
+            <p className="comics-description ">
+              {item.description || "Pas de description."}
+            </p>
+          </div>
+        </Link>
+        <Heart
+          className="card-heart"
+          style={{ fill: isFavorite ? "red" : "white" }}
+          onClick={isFavorite ? onRemoveFavorite : onAddFavoriteInLocalStorage}
+        />
+        {/* <button
         onClick={isFavorite ? onRemoveFavorite : onAddFavoriteInLocalStorage}
       >
         <FontAwesomeIcon icon="heart" color={isFavorite ? "red" : "gray"} />
-      </button>
-    </article>
+      </button> */}
+      </div>
+    </section>
   );
 };
 
