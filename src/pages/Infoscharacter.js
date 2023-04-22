8841ea44aa316c45899cdf23f8c737c1f5c5e2a5
oussamab <oussama@MacBook-Pro-de-oussamab.local>
2023-04-22 20:26:14 +0200

    change css

--- src/pages/Infoscharacter.js
+++ src/pages/Infoscharacter.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import { Oval } from "react-loader-spinner";
-import "../styles/InfosCharacter.css";
+import "../styles/Infoscharacter.css";
 
 const InfosCharacter = () => {
   const [data, setData] = useState();
